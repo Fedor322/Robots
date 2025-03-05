4221fc107d57126fa7c5cfa905eadbaded078d42
Fedor Grebnev <fedor.grebnev88@gmail.com>
2025-03-05 09:29:50 +0500

    beginmdi and customUi

---
+++ robots/src/main/java/gui/CustomTitleUi.java
@@ -0,0 +1,115 @@
+package gui;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class CustomTitleUi extends BasicInternalFrameTitlePane {
+
+    CustomTitleUi(JInternalFrame f) {
+        super(f);
+    }
+
+    @Override
+    protected void setButtonIcons() {
+        if (frame.isIcon()) {
+            setMinimizedIcons();
+        } else if (frame.isMaximum()) {
+            setMaximizedIcons();
+        } else {
+            setNormalIcons();
+        }
+        setCloseButtonIcon();
+    }
+
+    private void setMinimizedIcons() {
+        if (minIcon != null) {
+            iconButton.setIcon(minIcon);
+        }
+        iconButton.setToolTipText("Свернуть");
+        if (maxIcon != null) {
+            maxButton.setIcon(maxIcon);
+        }
+        maxButton.setToolTipText("Развернуть");
+    }
+
+    private void setMaximizedIcons() {
+        if (iconIcon != null) {
+            iconButton.setIcon(iconIcon);
+        }
+        iconButton.setToolTipText("Свернуть");
+        if (minIcon != null) {
+            maxButton.setIcon(minIcon);
+        }
+        maxButton.setToolTipText("Восстановить");
+    }
+
+    private void setNormalIcons() {
+        if (iconIcon != null) {
+            iconButton.setIcon(iconIcon);
+        }
+        iconButton.setToolTipText("Свернуть");
+        if (maxIcon != null) {
+            maxButton.setIcon(maxIcon);
+        }
+        maxButton.setToolTipText("Развернуть");
+    }
+
+    private void setCloseButtonIcon() {
+        if (closeIcon != null) {
+            closeButton.setIcon(closeIcon);
+        }
+        closeButton.setToolTipText("Закрыть");
+    }
+
+    @Override
+    protected void addSubComponents() {
+        removeAll();
+
+        setLayout(new BorderLayout());
+
+        JPanel menuPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        menuPanel.setOpaque(false);
+        menuPanel.add(createSystemMenuBar());
+
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 2, 0));
+        buttonPanel.setOpaque(false);
+        buttonPanel.add(iconButton);
+        buttonPanel.add(maxButton);
+        buttonPanel.add(closeButton);
+
+        add(menuPanel, BorderLayout.WEST);
+        add(buttonPanel, BorderLayout.EAST);
+    }
+
+
+    @Override
+    protected JMenuBar createSystemMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setLayout(new FlowLayout(FlowLayout.LEFT, -6, 0));
+        menuBar.add(createSystemMenu());
+        return menuBar;
+    }
+
+    private JMenuItem createMenuItem(String title, ActionListener listener) {
+        JMenuItem item = new JMenuItem(title);
+        item.setMargin(new Insets(0, -30, 0, 0));
+        item.addActionListener(listener);
+        return item;
+    }
+
+
+    @Override
+    protected JMenu createSystemMenu() {
+        JMenu systemMenu = new CustomMenu("");
+        systemMenu.setIcon(UIManager.getIcon("InternalFrame.icon"));
+        systemMenu.add(createMenuItem("Восстановить", restoreAction));
+        systemMenu.add(createMenuItem("Свернуть", iconifyAction));
+        systemMenu.add(createMenuItem("Развернуть", maximizeAction));
+        systemMenu.add(createMenuItem("Закрыть", closeAction));
+        return systemMenu;
+    }
+
+}
--- robots/src/main/java/gui/GameWindow.java
+++ robots/src/main/java/gui/GameWindow.java
@@ -2,19 +2,20 @@ package gui;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JInternalFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-public class GameWindow extends JInternalFrame
-{
+public class GameWindow extends JInternalFrame {
     private final GameVisualizer m_visualizer;
-    public GameWindow() 
-    {
-        super("Игровое поле", true, true, true, true);
+
+    public GameWindow() {
+        super("Поле", true, true, true, true);
         m_visualizer = new GameVisualizer();
+        setUI(new CustomInternalFrameUi(this));
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
     }
+
+
 }
